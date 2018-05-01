4d7929658d42c7999b2a57f743fe957945e492d1
sk-projects <skobox-github@yahoo.com>
2018-05-01 19:23:22 +0530

    corrections added script for test_dataset

--- script_dataset2.py
+++ script_dataset2.py
@@ -6,41 +6,41 @@ from fileinfo import FileList
 import pickle
 import os.path
 
-dataset1_dir = "/home/xena/Documents/dataset_paper/dataset2"
+dataset2_dir = "/home/xena/Documents/dataset_paper/dataset2"
 
-benign_dir = os.path.join(dataset1_dir, "benign")
-print(benign_dir)
-fl_benign_dir_win7 = FileList(benign_dir)
-print(fl_benign_dir_win7)
-fl_benign_dir_win7.setClass('Benign')
+# benign_dir = os.path.join(dataset1_dir, "benign")
+# print(benign_dir)
+# fl_benign_dir_win7 = FileList(benign_dir)
+# print(fl_benign_dir_win7)
+# fl_benign_dir_win7.setClass('Benign')
+#
+# benign_dir_win10 = os.path.join(dataset1_dir, "benign_win10")
+# print(benign_dir_win10)
+# fl_benign_dir_win10 = FileList(benign_dir_win10)
+# print(fl_benign_dir_win10)
+# fl_benign_dir_win10.setClass('Benign')
+#
+# fl_benign_dir = fl_benign_dir_win7 +  fl_benign_dir_win10
+# print(fl_benign_dir)
+# with open('benign_dataset2.pkl', 'wb') as output:
+#     pickle.dump(fl_benign_dir, output, pickle.HIGHEST_PROTOCOL)
+#
+# del fl_benign_dir_win7
+# del fl_benign_dir_win10
 
-benign_dir_win10 = os.path.join(dataset1_dir, "benign_win10")
-print(benign_dir_win10)
-fl_benign_dir_win10 = FileList(benign_dir_win10)
-print(fl_benign_dir_win10)
-fl_benign_dir_win10.setClass('Benign')
-
-fl_benign_dir = fl_benign_dir_win7 +  fl_benign_dir_win10
-print(fl_benign_dir)
-with open('benign_dataset2.pkl', 'wb') as output:
-    pickle.dump(fl_benign_dir, output, pickle.HIGHEST_PROTOCOL)
-
-del fl_benign_dir_win7
-del fl_benign_dir_win10
-
-trojan_dir = os.path.join(dataset1_dir, "malware", "trojan")
+trojan_dir = os.path.join(dataset2_dir, "malware", "trojan")
 print(trojan_dir)
 fl_trojan_dir = FileList(trojan_dir)
 print(fl_trojan_dir)
 fl_trojan_dir.setClass('Malware')
 
-worm_dir = os.path.join(dataset1_dir, "malware", "worm")
+worm_dir = os.path.join(dataset2_dir, "malware", "worm")
 print(worm_dir)
 fl_worm_dir = FileList(worm_dir)
 print(fl_worm_dir)
 fl_worm_dir.setClass('Malware')
 
-virus_dir = os.path.join(dataset1_dir, "malware", "virus")
+virus_dir = os.path.join(dataset2_dir, "malware", "virus")
 print(virus_dir)
 fl_virus_dir = FileList(virus_dir)
 print(fl_virus_dir)
@@ -55,8 +55,12 @@ del fl_trojan_dir
 del fl_worm_dir
 del fl_virus_dir
 
-dataset1 = fl_benign_dir + fl_malware_dir
-dataset1.generate_global_list()
-dataset1.generate_feature_vector(1000, 5)
-dataset1.save_feature_vector('dataset2_feature_vector.csv')
+with open(r'benign_dataset2.pkl', 'rb') as inputfile:
+    fl_benign_dir = pickle.load(inputfile)
+
+dataset2 = fl_benign_dir + fl_malware_dir
+print(dataset2)
+dataset2.generate_global_list()
+dataset2.generate_feature_vector(1000, 5)
+dataset2.save_feature_vector('dataset2_feature_vector.csv')
 
--- test_new_dataset.py
+++ test_new_dataset.py
@@ -1,11 +1,56 @@
 
 from mclearn import *
+from fileinfo import FileList
+import pickle
+import os.path
 
-url = 'dataset2_feature_vector.csv'
+# Generate test dataset
+
+test_dataset_dir = "/home/cloud/Documents/dataset_paper/test_dataset"
+
+benign_dir = os.path.join(test_dataset_dir, "benign")
+print(benign_dir)
+fl_benign_dir = FileList(benign_dir)
+print(fl_benign_dir)
+fl_benign_dir.setClass('Benign')
+with open('benign_test_dataset.pkl', 'wb') as output:
+    pickle.dump(fl_benign_dir, output, pickle.HIGHEST_PROTOCOL)
+
+trojan_dir = os.path.join(test_dataset_dir, "malware", "trojan")
+print(trojan_dir)
+fl_trojan_dir = FileList(trojan_dir)
+print(fl_trojan_dir)
+fl_trojan_dir.setClass('Malware')
+
+worm_dir = os.path.join(test_dataset_dir, "malware", "worm")
+print(worm_dir)
+fl_worm_dir = FileList(worm_dir)
+print(fl_worm_dir)
+fl_worm_dir.setClass('Malware')
+
+virus_dir = os.path.join(test_dataset_dir, "malware", "virus")
+print(virus_dir)
+fl_virus_dir = FileList(virus_dir)
+print(fl_virus_dir)
+fl_virus_dir.setClass('Malware')
+
+fl_malware_dir = fl_trojan_dir + fl_worm_dir + fl_virus_dir
+with open('malware_test_dataset.pkl', 'wb') as output:
+    pickle.dump(fl_malware_dir, output, pickle.HIGHEST_PROTOCOL)
+print(fl_malware_dir)
+
+test_dataset = fl_benign_dir + fl_malware_dir
+test_dataset.generate_global_list()
+test_dataset.generate_feature_vector(1000, 5)
+test_dataset.save_feature_vector('test_dataset_feature_vector.csv')
+
+
+# test machine learning models
+url = 'dataset1_feature_vector.csv'
 dataset = read_dataset(url)
 X_train, Y_train = select_features(dataset, 200)
 
-url = 'fv20180417-1.txt'
+url = 'test_dataset_feature_vector.csv'
 test_dataset = read_dataset(url)
 X_test, Y_test = select_features(test_dataset, 200)
 
