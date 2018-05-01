3b134da343ad944e8e8f7541b24a06d5624546f7
sk-projects <skobox-github@yahoo.com>
2018-05-01 17:13:04 +0530

    correction in script for dataset1

--- script_dataset1.py
+++ script_dataset1.py
@@ -18,19 +18,19 @@ with open('benign_dataset1.pkl', 'wb') as output:
 
 trojan_dir = os.path.join(dataset1_dir, "malware", "trojan")
 print(trojan_dir)
-fl_trojan_dir = FileList(benign_dir)
+fl_trojan_dir = FileList(trojan_dir)
 print(fl_trojan_dir)
 fl_trojan_dir.setClass('Malware')
 
 worm_dir = os.path.join(dataset1_dir, "malware", "worm")
 print(worm_dir)
-fl_worm_dir = FileList(benign_dir)
+fl_worm_dir = FileList(worm_dir)
 print(fl_worm_dir)
 fl_worm_dir.setClass('Malware')
 
 virus_dir = os.path.join(dataset1_dir, "malware", "virus")
 print(virus_dir)
-fl_virus_dir = FileList(benign_dir)
+fl_virus_dir = FileList(virus_dir)
 print(fl_virus_dir)
 fl_virus_dir.setClass('Malware')
 
