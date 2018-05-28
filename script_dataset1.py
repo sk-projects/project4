497e036e0ef96cf2f285d0fbdc5c067d950ba3e9
sk-projects <skobox-github@yahoo.com>
2018-05-28 18:49:31 +0530

    changes - added comments and description to malware detection component

--- script_dataset1.py
+++ script_dataset1.py
@@ -1,5 +1,19 @@
 """
-Script to generate feature vector for dataset1
+MALWARE DETECTION COMPONENT
+
+DATASET 1 : Trojan = 1576,   Virus = 2566,   Worm = 1234
+            Benign    =   1169
+            Total     =   6545
+
+Steps in this Component
+1. Extract Printable String Information from Binary Executable Files.
+2. Generate global list of strings
+3. Create unique list of strings
+4. Generate Binary Feature Vector
+    * Filter out strings less than length 4
+    * Sort the strings in descending order according to frequency count
+    * Select top 1000 strings
+5. Save Feature List for new Malware detection
 
 """
 from fileinfo import FileList
@@ -8,6 +22,7 @@ import os.path
 import os
 from mclearn import *
 
+# Initialize Directory and File Paths
 dataset1_dir = "/home/cloud/Documents/dataset_paper/dataset1"
 ds1_results_path = os.path.join(os.getcwd(),"dataset1_results")
 if not os.path.exists(ds1_results_path):
@@ -17,7 +32,9 @@ objfile_malware = os.path.join(ds1_results_path, 'malware_dataset1.pkl')
 objfile_dataset1 = os.path.join(ds1_results_path, 'dataset1.pkl')
 csv_dataset1_fv = os.path.join(ds1_results_path, 'dataset1_feature_vector.csv')
 file_fl_dataset1 = os.path.join(ds1_results_path, 'dataset1_feature_list.txt')
+var_ApplyMachineLearning = False
 
+# Read the directories containing sample executable files
 if not os.path.exists(objfile_benign):
     benign_dir = os.path.join(dataset1_dir, "benign")
     print(benign_dir)
@@ -28,7 +45,7 @@ if not os.path.exists(objfile_benign):
     with open(objfile_benign, 'wb') as output:
         pickle.dump(fl_benign_dir, output, pickle.HIGHEST_PROTOCOL)
 else:
-    print("reading benign dataset from pickle")
+    print("Reading benign dataset from pickle")
     with open(objfile_benign,'rb') as inputfile:
         fl_benign_dir = pickle.load(inputfile)
 
@@ -59,34 +76,51 @@ if not os.path.exists(objfile_malware):
         pickle.dump(fl_malware_dir, output, pickle.HIGHEST_PROTOCOL)
     print(fl_malware_dir)
 else:
-    print("reading malware dataset from pickle")
+    print("Reading malware dataset from pickle")
     with open(objfile_malware,'rb') as inputfile:
         fl_malware_dir =  pickle.load(inputfile)
 
 
+# Generate Global list of Unique Strings with frequency count
 dataset1 = fl_benign_dir + fl_malware_dir
 dataset1.generate_global_list()
 dataset1.calculate_unique_strings()
+
+# Generate Binary Feature Vector
 dataset1.generate_feature_vector(1000, 4)
+
+# Save top 1000 features into a list
 dataset1.save_feature_list(1000, 4, file_fl_dataset1)
 
+# Save feature vector
 if not os.path.exists(csv_dataset1_fv):
     dataset1.save_feature_vector(csv_dataset1_fv)
 
+# Save dataset
 if not os.path.exists(objfile_dataset1):
-    print("write dataset to pickle")
+    print("Write dataset to pickle")
     with open(objfile_dataset1, 'wb') as output:
         pickle.dump(dataset1, output, pickle.HIGHEST_PROTOCOL)
 
 
-# generate results for machine learning algorithms
-if 1 == 2:
-    print('reading feature vector dataset')
+# Generate results for machine learning algorithms
+if var_ApplyMachineLearning:
+    print('Reading Feature Vector Dataset')
+
+    # Read Dataset 1
     dataset = read_dataset(csv_dataset1_fv)
+
+    # Initialize to 7 machine learning models defined in mclearn [LR, KNN, DT, NB, MNB, SVM, RF]
     models = initialize_models()
+
+    # Intialize file names
     json_ds1_cv_results = os.path.join(ds1_results_path, "dataset1_cv_results.json")
     png_ds1_cv_graph = os.path.join(ds1_results_path, "dataset1_cv_graph.png")
+
+    # Cross Validation Results
     print('running machine learning algorithms')
     save_cv_results(dataset, models, 50, 1000, 50, json_ds1_cv_results)
+
+    # Plot Graph
     plot_graph('Cross Validation Results for Dataset1', 'Number of Features', 'Accuracy', json_ds1_cv_results,
                png_ds1_cv_graph)
